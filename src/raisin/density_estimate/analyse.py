8a8b59565fe262e03d985a6803e4305e99d58fc8
jjfaulkner <jamesjohnf@btinternet.com>
2025-04-16 17:52:54 +0100

    added functions to allow for strain estimation based on G position, width and I(2D)/I(G). Next job is to determine whether p or n from 2D and also make functions more modular.

--- src/raisin/density_estimate/analyse.py
+++ src/raisin/density_estimate/analyse.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy
-import os
+import matplotlib.pyplot as plt 
 
 from density_estimate.literature import *
 
@@ -53,9 +53,12 @@ def fit_peak(data: np.ndarray, xmin: float, xmax: float, func: callable, guess: 
 
 def analyse(file: str, delim: str) -> dict:
     data = read_data(file, delim)
+    fit_si = fit_peak(data, 500, 540, lorentzian, [1, 520, 5, 0, 0])
     fit_g = fit_peak(data, 1500, 1700, fano, [0.1, 1580, 10, -10, 0, 0])
     fit_2d = fit_peak(data, 2500, 2900, lorentzian, [0.1, 2680, 10, 0, 0])
-    return {'fit_g': fit_g[0], 'fit_2d': fit_2d[0]}
+    return {'fit_si': fit_si[0], 'fit_g': fit_g[0], 'fit_2d': fit_2d[0]}
+
+test = analyse('sample_data.txt', '\t')
 
 ##############
 # Estimate n #
@@ -67,12 +70,62 @@ def fit_i_2d_i_g(data):
     i2d_ig = i_2d / i_g
     solution_p = scipy.optimize.fsolve(lambda n: i2d_ig_spline(n) - i2d_ig, -1)
     solution_n = scipy.optimize.fsolve(lambda n: i2d_ig_spline(n) - i2d_ig, 3)
+    return solution_p*1e13, solution_n*1e13
+
+def fit_fwhm_g(data):
+    fwhm_g = 2*data['fit_g'][2]
+    fwhm_si = 2*data['fit_si'][2]
+    solution_p_ef = scipy.optimize.fsolve(lambda efermi: (fwhm_g_correction_slg_vectorised(efermi, 300, 0.13) - fwhm_g_correction_slg_vectorised(20, 300, 0.13)) - (fwhm_g - fwhm_si), -1)
+    solution_n_ef = scipy.optimize.fsolve(lambda efermi: (fwhm_g_correction_slg_vectorised(efermi, 300, 0.13) - fwhm_g_correction_slg_vectorised(20, 300, 0.13)) - (fwhm_g - fwhm_si), 1)
+
+    solution_p = scipy.optimize.fsolve(lambda n: e_fermi_slg(n) - solution_p_ef, -1e13)
+    solution_n = scipy.optimize.fsolve(lambda n: e_fermi_slg(n) - solution_n_ef, 1e13)
+    #solution_p = scipy.optimize.fsolve(lambda n: (fwhm_g_spline(n) - broadening_das) - (fwhm_g - fwhm_si), -1)
+    #solution_n = scipy.optimize.fsolve(lambda n: (fwhm_g_spline(n) - broadening_das) - (fwhm_g - fwhm_si), 3)
     return solution_p, solution_n
 
-test = analyse('sample_data.txt', '\t')
+###################
+# Estimate strain #
+###################
 
-density = fit_i_2d_i_g(test)
+def predict_pos_g(data, n, p):
+    expected_shift_p = pos_g_spline(n/1e13)
+    expected_shift_n = pos_g_spline(p/1e13)
+    #solution_p = scipy.optimize.fsolve(lambda n: (pos_g_correction_slg_vectorised(n, 300, 0.13) - pos_g_correction_slg_vectorised(20, 300, 0.13)) - (pos_g - pos_si), -1)
+    #solution_n = scipy.optimize.fsolve(lambda n: (pos_g_correction_slg_vectorised(n, 300, 0.13) - pos_g_correction_slg_vectorised(20, 300, 0.13)) - (pos_g - pos_si), 3)
+    return expected_shift_p, expected_shift_n
 
+def get_strain(pos, n, p):
+    diff_g_p = pos[0] - test['fit_g'][1]
+    diff_g_n = pos[1] - test['fit_g'][1]
+    strain_g_p = diff_g_p / 23 # from Mohiuddin for uniaxial
+    strain_g_n = diff_g_n / 23 # from Mohiuddin for uniaxial
+    return strain_g_p, strain_g_n
+
+##########
+# Script #
+##########
+
+density_iratio = fit_i_2d_i_g(test)
+
+print('Intensity Ratio')
 print(f"i_2d / i_g = {test['fit_2d'][0] / test['fit_g'][0]}")
-print(f"n_p = {density[0]}")
-print(f"n_n = {density[1]}")
+print(f"n_p = {density_iratio[0]}")
+print(f"n_n = {density_iratio[1]}")
+
+density_width = fit_fwhm_g(test)
+
+print('FWHM')
+print(f"FWHM(G) = {2*test['fit_g'][2]}")
+print(f"FWHM(Si) = {2*test['fit_si'][2]}")
+print(f"n_p = {density_width[0]}")
+print(f"n_n = {density_width[1]}")
+
+avg_p = (density_iratio[0] + density_width[0])/2
+avg_n = (density_iratio[1] + density_width[1])/2
+
+strain = get_strain(predict_pos_g(test, avg_n, avg_p), avg_n, avg_p)
+
+print('Strain')
+print(f"Strain(G) [p] = {strain[0]}")
+print(f"Strain(G) [n] = {strain[1]}")
--- src/raisin/density_estimate/literature.py
+++ src/raisin/density_estimate/literature.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.interpolate import splrep, BSpline # for smoothed interpolation
+from scipy import integrate
 import pandas as pd
 import os
+
 
 ################################################################
 # A script which fits splines to the data of Das et al. (2008) #
@@ -16,6 +18,8 @@ fwhm_g_data = pd.read_csv('~/Documents/raisin/src/raisin/density_estimate/data/f
 pos_g_data = pd.read_csv('~/Documents/raisin/src/raisin/density_estimate/data/pos_g_vs_n_das_expt.txt', delimiter='\t', header=None)
 pos_2d_data = pd.read_csv('~/Documents/raisin/src/raisin/density_estimate/data/pos_2d_vs_n_das_expt.txt', delimiter='\t', header=None)
 
+broadening_das = 2.5 # an estimate, based on comparison with theory from Lazzeri & Mauri (2006)
+
 # fitting smoothed splines to the data
 
 tck_ratio_i2d_ig = splrep(ratio_i2d_ig_data[0], ratio_i2d_ig_data[1], s = 0.02 * (len(ratio_i2d_ig_data[0]) - 2*np.sqrt(2*len(ratio_i2d_ig_data[0]))))
@@ -29,3 +33,47 @@ pos_g_spline = BSpline(*tck_pos_g)
 
 tck_pos_2d = splrep(pos_2d_data[0], pos_2d_data[1], s = (len(pos_2d_data[0]) - 2*np.sqrt(2*len(pos_2d_data[0]))))
 pos_2d_spline = BSpline(*tck_pos_2d)
+
+##########
+# Theory #
+##########
+
+h = 4.135e-15 # eV s
+c = 3e10 # cm/s
+k_boltzmann = 8.617e-5 # eV / K
+
+alpha_prime = 4.43e-3 # dimensionless
+#alpha_prime = 5.09e-4 # λ/2π (as in the ppt)
+# 2π α' is also called λ_Γ in literature
+E_ph = 0.196 # eV, equivalent photon energy E = hωc
+
+def f(epsilon, e_fermi, T): # defining the Fermi-Dirac distribution in the conventional way
+    return (1 / (np.exp((epsilon - e_fermi) / (k_boltzmann * T)) + 1))
+
+def pi_slg(e_fermi, T, delta): # the phonon self-energy for SLG. The 0.13i is an empirical broadening
+    epsilon = np.linspace(-10,10,20000)
+    return integrate.simpson(np.absolute(epsilon) * (f(epsilon, e_fermi, T) - f(-epsilon, e_fermi, T)) / (2*epsilon + complex(E_ph, delta)), epsilon) * alpha_prime
+
+def pos_g_correction_slg(e_fermi, T, delta): # Pos(G) comes from the real part of the self-energy
+    pi = pi_slg(e_fermi, T, delta)
+    result = (pi - pi_slg(0, T, delta)).real / (h * c)
+    return result
+
+def fwhm_g_correction_slg(e_fermi, T, delta): # FWHM(G) comes from the imaginary part of the self-energy
+    result = 2 * np.imag(pi_slg(e_fermi, T, delta) - pi_slg(0, T, delta)) / (h * c)
+    return -result
+
+pos_g_correction_slg_vectorised = np.vectorize(pos_g_correction_slg)
+fwhm_g_correction_slg_vectorised = np.vectorize(fwhm_g_correction_slg)
+
+##################
+# EF(n) from TBA #
+##################
+
+def e_fermi_slg(n):
+    hbar = h / (2*np.pi)
+    if n < 0: 
+        return - hbar * 1.1e8 * np.sqrt(np.pi * np.absolute(n))
+    else:
+        return hbar * 1.1e8 * np.sqrt(np.pi * np.absolute(n))
+e_fermi_slg_vectorised = np.vectorize(e_fermi_slg)
